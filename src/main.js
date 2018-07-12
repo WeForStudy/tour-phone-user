0e1435bf8c6192613acfe45a00237881e24e5a9c
frank994 <frank994wu@gmail.com>
2018-07-12 15:01:41 +0800

    ADD: ad vonic

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vonic from 'vonic'
 import App from './pages/app'
 import store from 'store'
-import router from './router'
+import routes from './router'
 import 'common/index.styl'
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   components: { App },
+//   template: '<App/>'
+// })
+Vue.use(Vonic.app, {
+  routes,
   store,
-  components: { App },
-  template: '<App/>'
 })
+
